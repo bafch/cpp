b0989ad7f2a230bac0ba9ccb1a0afb7547ad3b51
billyafernandez <billy@acklenavenue.com>
2018-06-21 15:05:43 -0600

    agregando ejercicios While

---
+++ 7-while/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+   int a = 1;
+
+   while( a <= 20 ) {
+      cout << "valor de a: " << a << endl;
+      a = a + 1;
+   }
+ 
+   return 0;
+}
+
