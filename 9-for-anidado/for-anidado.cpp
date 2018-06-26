b4adc70120741aca8d3381649bc629164b85dfbf
billyafernandez <billy@acklenavenue.com>
2018-06-25 18:43:11 -0600

    agregando proyecto matrix

--- 9-for-anidado/for-anidado.cpp
+++ 9-for-anidado/for-anidado.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
-using namespace std;
- 
-int main () {
-	
-	for(int a = 1; a <= 3; a = a + 1) {
-	
-	   for(int b = 1; b <= 5; b = b + 1) {
-	      cout << "valores: " << a << "." << b << endl;
-	   }
-	   
-	}
-	
-	cout << endl;
-	cout << endl;
-	cout << endl;
 
-	// imprime un arbol de *
-
-	int stars=1;
-	for(int line=0; line < 10; line++)
-	{
-		for(int s=0; s < stars; s++) {	
-			cout << "*";
-		}
-
-		stars++;
-		cout << endl;		
-	}
-
-	return 0;
+int Modulus(int iN, int iMod) {
+	int iQ = (iN/iMod);
+	return iN - (iQ*iMod);
 }
 
+char GetChar(int iGenerator, char cBase, int iRange) {
+	return (cBase + Modulus(iGenerator, iRange));
+}
 
+int main() {
+	system("COLOR 02");
+	char caRow[80];
+	int j = 7;
+	int k = 2;
+	int l = 5;
+	int m = 1;
+	while (true) {
+		int i = 0;
+		
+		// caracteres aleatorios
+		while (i < 80) {
+			if (caRow[i] != ' ') {
+				caRow[i] = GetChar(j + i*i, 33, 30);
+			}
+			std::cout << caRow[i];
+			++i;
+		}
+		j = (j + 31);
+		k = (k + 17);
+		l = (l + 47);
+		m = (m + 67);
+		caRow[Modulus(j, 80)] = '-';
+		caRow[Modulus(k, 80)] = ' ';
+		caRow[Modulus(l, 80)] = '-';
+		caRow[Modulus(m, 80)] = ' ';
+		
+		// incremente el vakor de 3000000 para retrasar el proceso
+		i = 0;
+		while (i < 3000000) {
+			GetChar(1, 1, 1);
+			 ++i;
+		}
+	}
+    return 0;
+}
 
---
+++ tareas/matrix/matrix.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int Modulus(int iN, int iMod) {
+	int iQ = (iN/iMod);
+	return iN - (iQ*iMod);
+}
+
+char GetChar(int iGenerator, char cBase, int iRange) {
+	return (cBase + Modulus(iGenerator, iRange));
+}
+
+int main() {
+	string constrasena;
+	
+	cout << "Eres un agente? ingresa tu contraseña ";
+	cin >> constrasena;
+	
+	if (constrasena == "smith") {
+		system("COLOR 02");
+
+		char caRow[80];
+		int j = 7;
+		int k = 2;
+		int l = 5;
+		int m = 1;
+
+		while (true) {
+			int i = 0;
+			
+			// caracteres aleatorios
+			while (i < 80) {
+				if (caRow[i] != ' ') {
+					caRow[i] = GetChar(j + i*i, 33, 30);
+				}
+				std::cout << caRow[i];
+				++i;
+			}
+			j = (j + 31);
+			k = (k + 17);
+			l = (l + 47);
+			m = (m + 67);
+			caRow[Modulus(j, 80)] = '-';
+			caRow[Modulus(k, 80)] = ' ';
+			caRow[Modulus(l, 80)] = '-';
+			caRow[Modulus(m, 80)] = ' ';
+			
+			// incremente el vakor de 3000000 para retrasar el proceso
+			i = 0;
+			while (i < 3000000) {
+				GetChar(1, 1, 1);
+				 ++i;
+			}
+		}		
+	} else {
+		cout << "acceso a la matrix denegado";
+	}
+
+    return 0;
+}
+
