81daf3f0a15126a9c506bc918b05f69fab789c87
billyafernandez <billy@acklenavenue.com>
2018-06-27 17:34:05 -0600

    agregando mas ejercicios For

---
+++ 10-for-con-while-if/for-while-if.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(int argc, char* argv[]) {	
+	cout << "Leer lista de argumentos con ciclo for" << endl;
+	cout << "--------------------------------------" << endl;
+	
+	for(int indice = 1; indice < argc; indice++) {
+		cout << "argumento " << indice << ": " << argv[indice] << endl;			
+	}
+	
+	cout << endl;
+	cout << endl;
+	cout << "Leer lista de argumentos con ciclo while" << endl;
+	cout << "----------------------------------------" << endl;
+	
+	int i = 1;
+	while (i < argc) {
+		cout << "argumento " << i << ": " << argv[i] << endl;
+		i++;
+	}
+	
+
+	cout << endl;
+	cout << endl;
+	cout << "Buscar todos los argumentos que contienen la letra A" << endl;
+	cout << "----------------------------------------------------" << endl;		
+	
+	for(int indice = 1; indice < argc; indice++) {
+		string texto = argv[indice];
+
+		if (texto.find("a", 0) != std::string::npos) {
+			cout << "argumento " << indice << ": " << argv[indice] << " <-- contiene una a" << endl;
+		} else {
+			cout << "argumento " << indice << ": " << argv[indice] << endl;			
+		}		
+	}	
+	
+	return 0;
+}
+
--- 9-for-anidado/for-anidado.cpp
+++ 9-for-anidado/for-anidado.cpp
@@ -1,48 +1,15 @@
 #include <iostream>
-
-int Modulus(int iN, int iMod) {
-	int iQ = (iN/iMod);
-	return iN - (iQ*iMod);
+using namespace std;
+ 
+int main () {
+   for(int a = 1; a <= 3; a = a + 1) {
+	   for(int b = 1; b <= 5; b = b + 1) {
+	      cout << "valor de a y b : " << a << "." << b << endl;
+	   }	             
+   }
+ 
+   return 0;
 }
 
-char GetChar(int iGenerator, char cBase, int iRange) {
-	return (cBase + Modulus(iGenerator, iRange));
-}
 
-int main() {
-	system("COLOR 02");
-	char caRow[80];
-	int j = 7;
-	int k = 2;
-	int l = 5;
-	int m = 1;
-	while (true) {
-		int i = 0;
-		
-		// caracteres aleatorios
-		while (i < 80) {
-			if (caRow[i] != ' ') {
-				caRow[i] = GetChar(j + i*i, 33, 30);
-			}
-			std::cout << caRow[i];
-			++i;
-		}
-		j = (j + 31);
-		k = (k + 17);
-		l = (l + 47);
-		m = (m + 67);
-		caRow[Modulus(j, 80)] = '-';
-		caRow[Modulus(k, 80)] = ' ';
-		caRow[Modulus(l, 80)] = '-';
-		caRow[Modulus(m, 80)] = ' ';
-		
-		// incremente el vakor de 3000000 para retrasar el proceso
-		i = 0;
-		while (i < 3000000) {
-			GetChar(1, 1, 1);
-			 ++i;
-		}
-	}
-    return 0;
-}
 
